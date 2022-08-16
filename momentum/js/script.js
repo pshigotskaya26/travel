d63a3562f440f68f88a989280ae398148ef5872a
pshigotskaya26 <pshigotskaya.tanya@tut.by>
2022-08-16 10:02:14 +0300

    fix: change url for urlQuotes

--- momentum/js/script.js
+++ momentum/js/script.js
@@ -230,7 +230,7 @@ const getRandomNumForQuotes = () => {
 //get random quote
 async function getQuotes() {
     //const urlQuotes = 'http://127.0.0.1:5500/js/data.json';
-    const urlQuotes = 'data.json';
+    const urlQuotes = '/data.json';
     const res = await fetch(urlQuotes);
 
     const data = await res.json();
